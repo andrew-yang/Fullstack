dd3cdc170b5214392783e7d916be38749be12edd
Andrew Yang <andrew.y.yang3@gmail.com>
2021-11-24 10:02:00 -0800

    Updates for part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,7 @@ import PersonForm from './Components/PersonForm'
 import Filter from './Components/Filter'
 import { filter } from './Components/Filter'
 import personService from './Services/persons'
+import Notification from './Components/Notification'
 
 const App = () => {
   const [ persons, setPersons] = useState([])
@@ -11,6 +12,8 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchTerm, setSearchTerm] = useState('')
+  const [ notification, setNotification] = useState('')
+  const [ notificationType, setNotificationType] = useState('')
 
   const hook = () => {
     console.log('effect')
@@ -28,6 +31,15 @@ const App = () => {
     setFilteredPersons(filter(searchTerm,newPersons))
   }
 
+  function notify(message, type){
+    setNotification(message)
+    setNotificationType(type)
+    setTimeout(() => {
+      setNotification(null)
+      setNotificationType('')
+    }, 5000)
+  }
+
   const SubmitHandler = (event) => {
     event.preventDefault()
     let existingPersons = persons.filter((person) => person.name === newName)
@@ -42,16 +54,20 @@ const App = () => {
         return;
       }
       personService.updateEntry(existingPersons[0].id, newPerson)
-      .then(newPerson => {
-        let newPersons = persons.filter(person => person.id != newPerson.id).concat(newPerson)
+      .then( updatedPerson => {
+        let newPersons = persons.filter(person => person.id !== updatedPerson.id).concat(updatedPerson)
         updateState(newPersons)
+        notify('Updated ' + newPerson.name, 'confirmation')
+      }).catch(error => {
+        notify('Could not update ' + newPerson.name, 'error')
       })
       
     } else {
       personService.createEntry(newPerson)
-      .then(newPerson => {
-        let newPersons = persons.concat(newPerson)
+      .then(createdPerson => {
+        let newPersons = persons.concat(createdPerson)
         updateState(newPersons)
+        notify('Created ' + newPerson.name, 'confirmation')
       })
     }
   }
@@ -61,10 +77,17 @@ const App = () => {
       return;
     }
     personService.deleteEntry(personToDelete.id)
-      .then(deletedPerson => {
+      .then( () => {
         let newPersons = persons.filter(existingPerson => existingPerson.id !== personToDelete.id)
-        console.log(newPersons, deletedPerson)
         updateState(newPersons)
+        notify('Deleted ' + personToDelete.name, 'confirmation')
+      }).catch(error => {
+        notify('Could not delete ' + personToDelete.name, 'error')
+        personService.getAllEntries()
+        .then(initialPersons => {
+          setPersons(initialPersons)
+          setFilteredPersons(initialPersons)
+        })
       })
   }
 
@@ -76,9 +99,10 @@ const App = () => {
    return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} type={notificationType} />
       <Filter searchChange={SearchChange} />
       <PersonForm numberChange={(event) => setNewNumber(event.target.value)} nameChange={(event) => setNewName(event.target.value)} submit={SubmitHandler} />
-      <PersonsView persons={filteredPersons} deleteHandler={DeleteHandler}/>
+      <PersonsView persons={filteredPersons} deleteHandler={DeleteHandler} />
     </div>
   )
 }
